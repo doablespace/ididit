f91393bb44841080999eb8326bb548e0ce1b34c5
Jan Joneš <jjones@outlook.cz>
2020-11-07 11:43:59 +0100

    Use database in activity list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ididit/data/database.dart';
 import 'package:ididit/screens/swiping_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,16 +11,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'I Did It',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          // Apply Raleway font throughout app.
-          textTheme: GoogleFonts.ralewayTextTheme(
-            Theme.of(context).textTheme,
-          )),
-      home: SwipingScreen(),
+    return Provider<Db>(
+      create: (_) => Db(),
+      child: MaterialApp(
+        title: 'I Did It',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            // Apply Raleway font throughout app.
+            textTheme: GoogleFonts.ralewayTextTheme(
+              Theme.of(context).textTheme,
+            )),
+        home: SwipingScreen(),
+      ),
     );
   }
 }
--- lib/widgets/activity_list.dart
+++ lib/widgets/activity_list.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:ididit/data/database.dart';
+import 'package:ididit/models/activity.dart';
+import 'package:provider/provider.dart';
 
 class ActivityList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 50,
-      child: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          if (index == 0) return ActivityButton(icon: Icon(Icons.add_rounded));
+      child: Consumer<Db>(builder: (context, db, child) {
+        return FutureProvider<List<Activity>>(
+          create: (_) async => db == null ? [] : db.activities,
+          child: Consumer<List<Activity>>(
+            builder: (context, activities, child) {
+              return ListView.builder(
+                itemCount: 1 + activities.length,
+                itemBuilder: (context, index) {
+                  if (index == 0)
+                    return ActivityButton(icon: Icon(Icons.add_rounded));
 
-          return ActivityButton(icon: Icon(Icons.accessibility_new_rounded));
-        },
-        scrollDirection: Axis.horizontal,
-      ),
+                  return ActivityButton(
+                      icon: Icon(Icons.accessibility_new_rounded));
+                },
+                scrollDirection: Axis.horizontal,
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }
