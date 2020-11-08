5c7146e1c6d5dedda6600266a74f187d8d94da69
Jan Joneš <jjones@outlook.cz>
2020-11-08 02:18:39 +0100

    Click "no activities" to add new activity

--- lib/widgets/current_activity.dart
+++ lib/widgets/current_activity.dart
@@ -3,6 +3,7 @@ import 'package:ididit/bloc/activities_bloc.dart';
 import 'package:ididit/models/activity.dart';
 import 'package:ididit/models/activity_states.dart';
 import 'package:ididit/models/model_provider.dart';
+import 'package:ididit/screens/edit_screen.dart';
 import 'package:ididit/ui/color_theme.dart';
 import 'package:ididit/widgets/activity_box.dart';
 import 'package:ididit/widgets/options_activity_box.dart';
@@ -35,9 +36,13 @@ class CurrentActivity extends StatelessWidget {
         // Show "no activities" placeholder.
         if (state == ActivitiesState.no_activities)
           return _ActivityColumn(
-            box: ActivityBox(
+            box: ClickableActivityBox(
               color: ActivityColors.accentGreen,
               child: Center(child: Icon(Icons.flaky_rounded, size: 180)),
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => EditScreen()));
+              },
             ),
             text: Text('No activities', style: activityTextStyle),
           );
