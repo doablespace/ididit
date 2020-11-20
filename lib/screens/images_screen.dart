71950497e6b6abce79f0b756507978c3fecabc46
Jan Joneš <jjones@outlook.cz>
2020-11-20 22:44:44 +0100

    Add custom similarity / edit distance (backup)

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,3 +1,5 @@
+import 'package:edit_distance/edit_distance.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:full_text_search/full_text_search.dart';
@@ -5,6 +7,7 @@ import 'package:full_text_search/term_search_result.dart';
 import 'package:ididit/bloc/activities_bloc.dart';
 import 'package:ididit/models/open_moji.dart';
 import 'package:provider/provider.dart';
+import 'package:string_similarity/string_similarity.dart';
 
 class ImagesScreen extends StatefulWidget {
   @override
@@ -46,12 +49,23 @@ class _ImagesScreenState extends State<ImagesScreen> {
 
           // Search OpenMojis.
           final query = (queryController.text ?? '').trim();
-          final fts = FullTextSearch<OpenMoji>(
+          final fts = FullTextSearch<OpenMoji>.scoring(
             term: query,
-            items: map.values,
+            items: Stream.fromIterable(map.values),
             tokenize: (openMoji) {
               return [openMoji.annotation, ...openMoji.getAllTags(map)];
             },
+            scorers: [
+              MatchAllTermsScoring(),
+              MatchedTokensScoring(),
+              MatchedTermsScoring(),
+              _SimilarityScoring(),
+            ],
+            matchers: [
+              EqualsMatch(),
+              StartsWithMatch(),
+              _SimilarMatch(),
+            ],
           );
 
           // Scroll to beginning when query changes.
@@ -102,5 +116,59 @@ class _ImagesScreenState extends State<ImagesScreen> {
         },
       ),
     );
+  }
+}
+
+class _SimilarMatch with TermMatcherMixin {
+  @override
+  List<TermMatch> apply<T>(FullTextSearch<T> search, TokenizedItem<T> item,
+      String term, Token token) {
+    term = term.toLowerCase();
+    // final similarity = StringSimilarity.compareTwoStrings(term, token.token);
+    // return [
+    //   if (similarity > 0.5) _SimilarTermMatch(term, token, similarity),
+    // ];
+    final distance = Levenshtein().normalizedDistance(term, token.token);
+    return [
+      if (distance < 0.2) _SimilarTermMatch(term, token, 1 - distance),
+    ];
+  }
+
+  @override
+  String get key => matchKey;
+  static const matchKey = 'similar';
+}
+
+class _SimilarTermMatch extends Equatable implements TermMatch {
+  @override
+  final Token matchedToken;
+
+  @override
+  final String term;
+
+  final double similarity;
+
+  _SimilarTermMatch(this.term, this.matchedToken, this.similarity);
+
+  @override
+  String get key => _SimilarMatch.matchKey;
+
+  @override
+  List<Object> get props => [key, matchedToken, term];
+}
+
+class _SimilarityScoring extends SearchScoring {
+  @override
+  void scoreTerm(FullTextSearch search, TermSearchResult term, Score current) {
+    for (final t in term.matchedTokens) {
+      switch (t.key) {
+        case _SimilarMatch.matchKey:
+          current += Boost.amount(
+            (t as _SimilarTermMatch).similarity,
+            "tokenSimilar",
+          );
+          break;
+      }
+    }
   }
 }
