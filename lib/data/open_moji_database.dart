72e3e1be4c36c28dc2b4ae8e738dac11195c85ec
Jan Joneš <jjones@outlook.cz>
2020-11-24 13:33:35 +0100

    Move OpenMoji search out of UI

--- lib/data/open_moji_database.dart
+++ lib/data/open_moji_database.dart
@@ -1,6 +1,10 @@
 import 'package:csv/csv.dart';
 import 'package:csv/csv_settings_autodetection.dart';
+import 'package:edit_distance/edit_distance.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/services.dart';
+import 'package:full_text_search/full_text_search.dart';
+import 'package:full_text_search/term_search_result.dart';
 import 'package:ididit/extensions.dart';
 import 'package:ididit/models/open_moji.dart';
 
@@ -25,5 +29,87 @@ class OpenMojiDatabase {
           key: (openMoji) => openMoji.emoji);
       return OpenMojiDatabase._(map, openMojis.toList());
     });
+  }
+
+  OpenMojiSearch search(String query) => OpenMojiSearch(this, query);
+}
+
+class OpenMojiSearch {
+  final FullTextSearch<OpenMoji> fts;
+
+  OpenMojiSearch(OpenMojiDatabase db, String query)
+      : fts = FullTextSearch<OpenMoji>.scoring(
+          term: query,
+          items: Stream.fromIterable(db.list),
+          tokenize: (openMoji) {
+            return [openMoji.annotation, ...openMoji.getAllTags(db.map)];
+          },
+          scorers: [
+            MatchAllTermsScoring(),
+            MatchedTokensScoring(),
+            MatchedTermsScoring(),
+            _SimilarityScoring(),
+          ],
+          matchers: [
+            EqualsMatch(),
+            StartsWithMatch(),
+            _SimilarMatch(),
+          ],
+        );
+}
+
+class _SimilarMatch with TermMatcherMixin {
+  @override
+  List<TermMatch> apply<T>(FullTextSearch<T> search, TokenizedItem<T> item,
+      String term, Token token) {
+    if (term.length < 4 || token.token.length < 4) return const [];
+    term = term.toLowerCase();
+    // final similarity = StringSimilarity.compareTwoStrings(term, token.token);
+    // return [
+    //   if (similarity > 0.5) _SimilarTermMatch(term, token, similarity),
+    // ];
+    final distance = Levenshtein().distance(term, token.token);
+    return [
+      if (distance > 0 && distance < 3)
+        _SimilarTermMatch(term, token, 1 - (distance - 1) / 2),
+    ];
+  }
+
+  @override
+  String get key => matchKey;
+  static const matchKey = 'similar';
+}
+
+class _SimilarTermMatch extends Equatable implements TermMatch {
+  @override
+  final Token matchedToken;
+
+  @override
+  final String term;
+
+  final double similarity;
+
+  _SimilarTermMatch(this.term, this.matchedToken, this.similarity);
+
+  @override
+  String get key => _SimilarMatch.matchKey;
+
+  @override
+  List<Object> get props => [key, matchedToken, term];
+}
+
+class _SimilarityScoring extends SearchScoring {
+  @override
+  void scoreTerm(FullTextSearch search, TermSearchResult term, Score current) {
+    for (final t in term.matchedTokens) {
+      switch (t.key) {
+        case _SimilarMatch.matchKey:
+          current += Boost.amount(
+            (t as _SimilarTermMatch).similarity,
+            "tokenSimilar",
+          );
+          break;
+      }
+    }
   }
 }
--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,13 +1,9 @@
-import 'package:edit_distance/edit_distance.dart';
-import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:full_text_search/full_text_search.dart';
 import 'package:full_text_search/term_search_result.dart';
 import 'package:ididit/bloc/activities_bloc.dart';
 import 'package:ididit/models/open_moji.dart';
 import 'package:provider/provider.dart';
-import 'package:string_similarity/string_similarity.dart';
 
 class ImagesScreen extends StatefulWidget {
   @override
@@ -49,24 +45,7 @@ class _ImagesScreenState extends State<ImagesScreen> {
 
           // Search OpenMojis.
           final query = (queryController.text ?? '').trim();
-          final fts = FullTextSearch<OpenMoji>.scoring(
-            term: query,
-            items: Stream.fromIterable(map.values),
-            tokenize: (openMoji) {
-              return [openMoji.annotation, ...openMoji.getAllTags(map)];
-            },
-            scorers: [
-              MatchAllTermsScoring(),
-              MatchedTokensScoring(),
-              MatchedTermsScoring(),
-              _SimilarityScoring(),
-            ],
-            matchers: [
-              EqualsMatch(),
-              StartsWithMatch(),
-              _SimilarMatch(),
-            ],
-          );
+          final fts = activitiesBloc.openMojis.search(query).fts;
 
           // Scroll to beginning when query changes.
           if (scrollController.hasClients && previousQuery != query)
@@ -116,61 +95,5 @@ class _ImagesScreenState extends State<ImagesScreen> {
         },
       ),
     );
-  }
-}
-
-class _SimilarMatch with TermMatcherMixin {
-  @override
-  List<TermMatch> apply<T>(FullTextSearch<T> search, TokenizedItem<T> item,
-      String term, Token token) {
-    if (term.length < 4 || token.token.length < 4) return const [];
-    term = term.toLowerCase();
-    // final similarity = StringSimilarity.compareTwoStrings(term, token.token);
-    // return [
-    //   if (similarity > 0.5) _SimilarTermMatch(term, token, similarity),
-    // ];
-    final distance = Levenshtein().distance(term, token.token);
-    return [
-      if (distance > 0 && distance < 3)
-        _SimilarTermMatch(term, token, 1 - (distance - 1) / 2),
-    ];
-  }
-
-  @override
-  String get key => matchKey;
-  static const matchKey = 'similar';
-}
-
-class _SimilarTermMatch extends Equatable implements TermMatch {
-  @override
-  final Token matchedToken;
-
-  @override
-  final String term;
-
-  final double similarity;
-
-  _SimilarTermMatch(this.term, this.matchedToken, this.similarity);
-
-  @override
-  String get key => _SimilarMatch.matchKey;
-
-  @override
-  List<Object> get props => [key, matchedToken, term];
-}
-
-class _SimilarityScoring extends SearchScoring {
-  @override
-  void scoreTerm(FullTextSearch search, TermSearchResult term, Score current) {
-    for (final t in term.matchedTokens) {
-      switch (t.key) {
-        case _SimilarMatch.matchKey:
-          current += Boost.amount(
-            (t as _SimilarTermMatch).similarity,
-            "tokenSimilar",
-          );
-          break;
-      }
-    }
   }
 }
